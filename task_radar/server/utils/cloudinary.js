f975f4c8687d3edf1a48e7a98b7d783e2dbfc15e
ZaidP101 <zpatel044@gmail.com>
2025-04-20 01:35:15 +0530

    Cloudinary and env setup

--- task_radar/server/utils/cloudinary.js
+++ task_radar/server/utils/cloudinary.js
@@ -1,4 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+import { response } from 'express';
 
 
 
@@ -8,3 +10,19 @@ cloudinary.config({
     api_key: CLOUDINARY_API_KEY, 
     api_secret: CLOUDINARY_CLOUD_SECRET // Click 'View API Keys' above to copy your API secret
 });
+
+// Upload an image
+const uploadCloudinary = async(localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+        cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        console.log("file uploaded", response.url);
+    } catch (error) {
+        FileSystem.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export{uploadCloudinary}
